9c79c94388fc390de32b2c6e5e819e808600e099
4ard <112971420+4ard@users.noreply.github.com>
2024-10-15 17:03:13 +0300

    Пункт 4
    Сделать методы класса (статические) валидации всех необходимых полей. Сделать так, чтобы существование объекта с неразрешёнными полями было невозможно.
    
    Каждый метод валидации проверяет, что соответствующее поле не пустое и является строкой. Если проверка не пройдена, выбрасывается исключение ValueError.
    
    При создании объекта вызываются методы валидации для всех полей, что предотвращает создание объекта с недопустимыми значениями.

--- Lr1/Customer.py
+++ Lr1/Customer.py
@@ -1,9 +1,9 @@
 class Customer:
     def __init__(self, name, address, phone, contact_person):
-        self.__name = name
-        self.__address = address
-        self.__phone = phone
-        self.__contact_person = contact_person
+        self.__name = self.validate_name(name)
+        self.__address = self.validate_address(address)
+        self.__phone = self.validate_phone(phone)
+        self.__contact_person = self.validate_contact_person(contact_person)
 
     # Геттеры
     def get_name(self):
@@ -20,16 +20,41 @@ class Customer:
 
     # Сеттеры
     def set_name(self, name):
-        self.__name = name
+        self.__name = self.validate_name(name)
 
     def set_address(self, address):
-        self.__address = address
+        self.__address = self.validate_address(address)
 
     def set_phone(self, phone):
-        self.__phone = phone
+        self.__phone = self.validate_phone(phone)
 
     def set_contact_person(self, contact_person):
-        self.__contact_person = contact_person
+        self.__contact_person = self.validate_contact_person(contact_person)
+
+    # Статические методы валидации
+    @staticmethod
+    def validate_name(name):
+        if not name or not isinstance(name, str):
+            raise ValueError("Имя должно быть непустой строкой.")
+        return name
+
+    @staticmethod
+    def validate_address(address):
+        if not address or not isinstance(address, str):
+            raise ValueError("Адрес должен быть непустой строкой.")
+        return address
+
+    @staticmethod
+    def validate_phone(phone):
+        if not phone or not isinstance(phone, str):
+            raise ValueError("Телефон должен быть непустой строкой.")
+        return phone
+
+    @staticmethod
+    def validate_contact_person(contact_person):
+        if not contact_person or not isinstance(contact_person, str):
+            raise ValueError("Контактное лицо должно быть непустой строкой.")
+        return contact_person
 
     def __str__(self):
         return f"Заказчик: {self.__name}, Адрес: {self.__address}, Телефон: {self.__phone}, Контактное лицо: {self.__contact_person}"
